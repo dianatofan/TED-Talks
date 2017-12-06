ac414e2078159dfcf3d8a2cc0d23bb3ed44d6b6f
Diana Tofan <s172393@student.dtu.dk>
2017-12-06 17:41:07 +0100

    Updates

--- React/ted-talks/src/Content.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Content.css';
-
-const Content = () =>
-  <div>
-  </div>;
-
-export default Content;
--- React/ted-talks/src/Notebook.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Notebook = () =>
-  <div>Notebook</div>;
-
-export default Notebook;
